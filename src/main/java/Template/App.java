7e85f768ff0d60c664ca6615f0bd3ff09a87f64a
shli7029 <shli7029@uni.sydney.edu.au>
2020-06-28 22:15:23 +1000

    template method

---
+++ src/main/java/Template/App.java
@@ -0,0 +1,15 @@
+package Template;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Iphone iphone = new Iphone();
+        iphone.createPhone();
+        System.out.println("--------------------");
+        AndroidPhone androidPhone = new AndroidPhone();
+        androidPhone.createPhone();
+
+    }
+
+}
