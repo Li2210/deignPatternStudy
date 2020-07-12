f2953d7e216faee4dbbe779d7e9751afef2c1c92
shli7029 <shli7029@uni.sydney.edu.au>
2020-07-12 23:32:26 +1000

    proxy pattern

---
+++ src/main/java/proxy/App.java
@@ -0,0 +1,9 @@
+package proxy;
+
+public class App {
+    public static void main(String[] args) {
+        subject real = new RealSubject();
+        subject proxy = new ComputerProxy(real);
+        proxy.play();
+    }
+}
